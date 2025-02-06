e55ab6926bf9ef3f31e5ec71e730a7671e7ed341
Viktor Shen <viktorshenofficial@gmail.com>
2025-02-06 01:17:23 +0100

    built dist

---
+++ dist/components/AnimatedMarkdown.d.ts
@@ -0,0 +1,21 @@
+import React from 'react';
+import './animations.css';
+import './custom-lists.css';
+interface SmoothTextProps {
+    content: string;
+    sep?: string;
+    animation?: string | null;
+    animationDuration?: string;
+    animationTimingFunction?: string;
+    codeStyle?: any;
+    htmlComponents?: any;
+    customComponents?: any;
+}
+interface CustomRendererProps {
+    rows: any[];
+    stylesheet: any;
+    useInlineStyles: boolean;
+}
+export declare const customCodeRenderer: ({ animation, animationDuration, animationTimingFunction }: any) => ({ rows, stylesheet, useInlineStyles }: CustomRendererProps) => React.JSX.Element[];
+declare const MarkdownAnimateText: React.FC<SmoothTextProps>;
+export default MarkdownAnimateText;
